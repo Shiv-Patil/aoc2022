5ca44481361e8541d5fe99ab3d81eb20c5444f2e
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2022-12-04 16:22:46 +0530

    add day 4

---
+++ d04_1.py
@@ -0,0 +1,8 @@
+overlaps = 0
+while inp := input():
+    first, second = map(lambda x: (*map(int, x.split("-")),), inp.split(","))
+    if (first[0] >= second[0] and first[1] <= second[1]) or (
+        second[0] >= first[0] and second[1] <= first[1]
+    ):
+        overlaps += 1
+print(overlaps)
