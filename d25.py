484cc8c98a1ea1d8522ea585bbd98055c6f40c08
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2022-12-25 16:23:00 +0530

    day 25 bugfix

--- d25.py
+++ d25.py
@@ -1,4 +1,16 @@
-SNAFU = {5: ("1", "0"), 4: ("1", "-"), 3: ("1", "="), "2": 2, "1": 1, "0": 0, "-": -1, "=": -2, -3: ("-", "2"), -4: ("-", "1"), -5: ("-", "0")}
+SNAFU = {
+    5: ("1", "0"),
+    4: ("1", "-"),
+    3: ("1", "="),
+    "2": 2,
+    "1": 1,
+    "0": 0,
+    "-": -1,
+    "=": -2,
+    -3: ("-", "2"),
+    -4: ("-", "1"),
+    -5: ("-", "0"),
+}
 data = open("25.in", "r").read().strip().split("\n")
 
 cur = data.pop()
@@ -9,13 +21,16 @@ for num in data:
     elif len(num) > len(cur):
         cur = cur.rjust(len(num), "0")
     else:
-        num = num.rjust(len(cur) + 1, "0")
-        cur = cur.rjust(len(num) + 1, "0")
-    
+        num = num.rjust(len(num) + 1, "0")
+        cur = cur.rjust(len(cur) + 1, "0")
+
     res = ""
-    for c in range(-1, -len(cur)-1, -1):
+    for c in range(-1, -len(cur) - 1, -1):
         addition = SNAFU[cur[c]] + SNAFU[num[c]] + SNAFU[carry]
-        carry, next = SNAFU.get(addition, ("0", str(addition) if addition >= 0 else "-" if addition == -1 else "="))
+        carry, next = SNAFU.get(
+            addition,
+            ("0", str(addition) if addition >= 0 else "-" if addition == -1 else "="),
+        )
         res = next + res
     cur = res.lstrip("0")
 
